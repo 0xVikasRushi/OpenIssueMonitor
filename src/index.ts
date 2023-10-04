fffbd861591225550983ec2c14ae8a651382168c
0xvikasrushi <0xvikas@gmail.com>
2023-10-04 18:16:44 +0530

    📦 NEW: project init

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+require("dotenv").config();
+import { Octokit } from "@octokit/core";
+
+var cron = require("node-cron");
+const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_TOKEN,
+});
+
+async function getIssues(repoOwner: string, repoName: string) {
+  try {
+    const req = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+      owner: repoOwner,
+      repo: repoName,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+    console.log(req.data[0].url);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+cron.schedule("*/15 * * * * *", () => {
+  console.count(
+    "running a task every 45 seconds------------------------------------>"
+  );
+  getIssues("facebook", "react");
+});
