d6650735018473c8e13a03b523a9716bd1bf9de3
0xvikasrushi <0xvikas@gmail.com>
2023-10-06 00:57:15 +0530

    📦 NEW: /rate-limit command to telegram bot

--- utils/telegram.ts
+++ utils/telegram.ts
@@ -1,7 +1,18 @@
 import TelegramBot from "node-telegram-bot-api";
 import { TELEGRAM_ACCESS_TOKEN } from "./constant";
+import { getRateLimit } from "./../src/api";
 
 console.log("Telegram bot server started...");
 const bot = new TelegramBot(TELEGRAM_ACCESS_TOKEN, { polling: true });
+
+bot.on("message", async (msg) => {
+  if (msg.text === "/rate-limit") {
+    const rate_limit = await getRateLimit();
+    bot.sendMessage(
+      msg.chat.id,
+      `Remaining: ${rate_limit.remaining}\nReset time: ${rate_limit.resetTime}`
+    );
+  }
+});
 
 export default bot;
